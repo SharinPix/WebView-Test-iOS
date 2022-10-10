18f873f863dc00467b0c07572322d84c21a63276
zfir <zafir.heerah@spoonconsulting.com>
2022-10-10 17:17:52 +0400

    Initial Commit

---
+++ WebView Test/WebView_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_TestApp.swift
+//  WebView Test
+//
+//  Created by Zafir on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WebView_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
